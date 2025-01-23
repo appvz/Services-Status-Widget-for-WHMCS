9dee666290a7432ce1de9d0f158593212288fcfe
AppVZ Online <admin@appvz.com>
2025-01-23 09:28:21 +0700

    Update ServicesStatusWidget.php
    Add active, expired domain number

--- ServicesStatusWidget.php
+++ ServicesStatusWidget.php
@@ -6,9 +6,8 @@ add_hook('AdminHomeWidgets', 1, function() {
 
 /**
  * Services Status Widget.
- *
- * @copyright Copyright (c) AppVZ Online 2025
- * @license https://appvz.com
+ * @copyright Copyright (c) AppVZ Online
+ * @license https://appvz.com, https://github.com/appvz/Services-Status-Widget-for-WHMCS
  */
  
  use WHMCS\Database\Capsule;
@@ -32,22 +31,25 @@ class ServicesStatusWidget extends \WHMCS\Module\AbstractWidget
         // Fetch data from the database
         $activeServices = Capsule::table('tblhosting')->where('domainstatus', 'Active')->count();
         $suspendedServices = Capsule::table('tblhosting')->where('domainstatus', 'Suspended')->count();
-        
+        $activeDomains = Capsule::table('tbldomains')->where('status', 'Active')->count();
+        $expiredDomains = Capsule::table('tbldomains')->where('status', 'Expired')->count();
         return '
 <div class="icon-stats">
     <div class="row">
         <div class="col-sm-6">
             <div class="item">
                 <div class="icon-holder text-center color-green">
-                    <i class="pe-7s-check"></i>
+                    <a href="services?status=active"><i class="pe-7s-check"></i></a>
                 </div>
                 <div class="data">
                     <div class="note">
-                        Active Services
+                        <a href="services?status=active">Active Services</a>
                     </div>
                     <div class="number">
+							<a href="services?status=active">
                             <span class="color-green">' . $activeServices . '</span>
                             <span class="unit">Active</span>
+							</a>
                     </div>
                 </div>
             </div>
@@ -65,6 +67,42 @@ class ServicesStatusWidget extends \WHMCS\Module\AbstractWidget
 						<a href="services?status=suspended">
                         <span class="color-orange">' . $suspendedServices . '</span>
                         <span class="unit">Suspended</span>
+						</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+		<div class="col-sm-6">
+            <div class="item">
+                <div class="icon-holder text-center color-green">
+                    <a href="domains?status=active"><i class="pe-7s-global"></i></a>
+                </div>
+                <div class="data">
+                    <div class="note">
+                        <a href="domains?status=active">Active Domains</a>
+                    </div>
+                    <div class="number">
+						<a href="domains?status=active">
+                        <span class="color-green">' . $activeDomains . '</span>
+                        <span class="unit">Active</span>
+						</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+		<div class="col-sm-6">
+            <div class="item">
+                <div class="icon-holder text-center">
+                    <a href="domains?status=expired"><i class="pe-7s-less"></i></a>
+                </div>
+                <div class="data">
+                    <div class="note">
+                        <a href="domains?status=expired">Expired Domains</a>
+                    </div>
+                    <div class="number">
+						<a href="domains?status=expired">
+                        <span>' . $expiredDomains . '</span>
+                        <span class="unit">Expired</span>
 						</a>
                     </div>
                 </div>
